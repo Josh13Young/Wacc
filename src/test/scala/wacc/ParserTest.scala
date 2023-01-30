0648b872abbcfb9419f6ce9ce455a895920dac19
Joshua Young <jy2221@aube.doc.ic.ac.uk>
2023-01-30 13:00:37 +0000

    [jy221] added first test case

--- src/test/scala/wacc/ParserTest.scala
+++ src/test/scala/wacc/ParserTest.scala
@@ -2,19 +2,32 @@ package wacc
 
 import org.scalatest._
 import flatspec._
-import matchers._
-import org.scalatest.matchers.must.Matchers.{a, not}
+import org.scalatest.matchers.must.Matchers.{not, be, equal, eq}
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 import parsley.Parsley._
+import parsley.{Failure, Success}
 import parsley.Success
 import parsley.implicits.character.{charLift, stringLift}
 
 class ParserTest extends AnyFlatSpec {
 
-  "example test..." should "fail if no input" in {
-    'a'.parse("") should not be a [Success[_]]
+  "begin return 3 end" should "succeed and return IntLiter(3)" in {
+    val input = "begin return 3 end"
+    val output = parseOutput(input)   
+
+    val answer = "Program(List(),List(Return(IntLiter(3))))"
+
+    output shouldEqual answer;
   }
 
+  private def parseOutput(input: String) : String = {
+    var output = ""
+    parser.parser.parse(input) match {
+            case Success(x) => output = (s"$x")
+            case Failure(msg) => output = (msg)
+        }
+    return output
+  }
 
 }
