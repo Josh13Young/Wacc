335f6f3406338212d1c8cee09ef9d7609c4e0201
Rickie Ma <rm521@ic.ac.uk>
2023-02-07 12:20:07 +0000

    [rm521] new main file with semantic errors

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -6,15 +6,19 @@ import wacc.error.WaccErrorBuilder
 object Main {
   def main(args: Array[String]): Unit = {
     val source = scala.io.Source.fromFile(args.head)
-    val input = try source.getLines().toList.mkString("\n") finally source.close()
+    val inputList = try source.getLines().toList finally source.close()
+    val input = inputList.mkString("\n")
     implicit val eb: WaccErrorBuilder = new WaccErrorBuilder
     parser.parser.parse(input) match {
       case Success(x) =>
         println(s"$input = $x")
-        if (x.check(new SymbolTable(None))) {
+        val seb = new wacc.error.WaccSemanticErrorBuilder.SemanticError
+        seb.program = inputList
+        if (x.check(new SymbolTable(None))(seb)) {
           println("Program is semantically correct")
         } else {
           println("Program is semantically incorrect")
+          println(seb.errors)
            sys.exit(200)
         }
       case Failure(msg) =>
