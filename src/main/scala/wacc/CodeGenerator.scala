e3931838524f7b160fef4cbb79dc8a608a4d36ad
Rickie Ma <rm521@ic.ac.uk>
2023-02-13 13:11:43 +0000

    [rm521] implements code gen for exit

--- src/main/scala/wacc/CodeGenerator.scala
+++ src/main/scala/wacc/CodeGenerator.scala
@@ -1,13 +1,13 @@
 package wacc
 
-import wacc.ast.{ASTNode, Program, Skip}
+import wacc.ast._
 
 object CodeGenerator {
 
   def generate(ast: ASTNode): String = {
     val start = ".data\n.text\n.global main\n"
     val mainStart = "main:\n\tpush {fp, lr}\n\tmov fp, sp\n"
-    val mainEnd = "\tmov r0, #0\n\tpop {fp, pc}"
+    val mainEnd = "\tmov r0, #0\n\tpop {fp, pc}\n"
     val sb = new StringBuilder
     ast match {
       case Program(functions, stat) =>
@@ -15,8 +15,18 @@ object CodeGenerator {
         sb.append(mainStart)
         stat.map(s => sb.append(generate(s)))
         sb.append(mainEnd)
+      case Exit(expr) =>
+        sb.append(exprGen(expr))
       case _ =>
     }
     sb.toString()
+  }
+
+  private def exprGen(expr: Expr): String = {
+    expr match {
+      case IntLiter(value) =>
+        s"\tmov r8, #$value\n\tmov r0, r8\n\tbl exit\n"
+      case _ => ""
+    }
   }
 }
