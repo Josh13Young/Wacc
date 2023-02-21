c0d9c4ac4726d08ca705ae8def20aeabe07c81aa
Joshua Young <jy2221@ic.ac.uk>
2023-02-21 16:36:04 +0000

    [jy2221] added helper function to the code gen test

--- src/test/scala/wacc/CodeGenTest/Code_Gen_Test.scala
+++ src/test/scala/wacc/CodeGenTest/Code_Gen_Test.scala
@@ -1,21 +1,39 @@
 package wacc
 
 import org.scalatest.flatspec._
+import org.scalatest.matchers.must.Matchers.matchPattern
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import parsley.{Failure, Success}
 import wacc.backend.CodeGenerator
 import wacc.backend.CodeGenerator.{generate, generateString}
 import wacc.frontend.{SymbolTable, parser}
 
-import java.io.PrintWriter
+import java.io.{File, PrintWriter}
+import scala.language.postfixOps
 import sys.process._
 
 class Code_Gen_Test extends AnyFlatSpec {
 
   "test" should "succeed" in {
+    val testlist = "wacc_examples/valid/array"
+    val files = getListOfFiles(testlist)
 
-    val file = "wacc_examples/valid/expressions/addExpr.wacc"
 
+    println(0)
+    for (file <- files) {
+      println(file)
+      println(1)
+    }
+
+
+    //val output = helper(file)
+    //println(output)
+    1 shouldBe(1)
+
+  }
+
+  private def helper(file: String): String = {
+    var output = ""
     val source = scala.io.Source.fromFile(file)
     val inputList = try source.getLines().toList finally source.close()
     val input = inputList.mkString("\n")
@@ -32,15 +50,36 @@ class Code_Gen_Test extends AnyFlatSpec {
           pw.write(code)
           pw.close()
 
-          val output = "./test.sh" !!
-          println(output)
-          1 shouldBe 1
+          val output = ("./test.sh" !!)
+          output
+        } else {
+          seb.printAll()
+          if (seb.isSemantic) {
+            sys.exit(200)
+          } else {
+            sys.exit(100)
+          }
         }
 
       case Failure(msg) =>
         // Should throw error if parser failed
-        true shouldBe false
+        return "";
     }
+  }
+
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
+  def getInput(file: File): String = {
+    val source = scala.io.Source.fromFile(file)
+    val input = try source.getLines().toList.mkString("\n") finally source.close()
+    input
   }
 
 }
