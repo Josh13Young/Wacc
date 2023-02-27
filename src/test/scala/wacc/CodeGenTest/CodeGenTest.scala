c670df219ffcd0617b7d2838ffee9e6d9e041287
Joshua Young <jy2221@ic.ac.uk>
2023-02-27 21:23:25 +0000

    [jy2221] added tests for arrays, basics, advanced, function, and if wacc examples

--- src/test/scala/wacc/CodeGenTest/CodeGenTest.scala
+++ src/test/scala/wacc/CodeGenTest/CodeGenTest.scala
@@ -5,7 +5,27 @@ import wacc.helperFunction.assemblyRunFolder
 
 class CodeGenTest extends AnyFlatSpec {
 
-  "Expr" should "succeed and print correct output" in {
+  "advanced" should "succeed and print correct output" in {
+    assemblyRunFolder("wacc_examples/valid/advanced")
+  }
+
+  "array" should "succeed and print correct output" in {
+    assemblyRunFolder("wacc_examples/valid/array")
+  }
+
+  "basic" should "succeed and print correct output" in {
+    assemblyRunFolder("wacc_examples/valid/basic")
+  }
+
+  "expressions" should "succeed and print correct output" in {
     assemblyRunFolder("wacc_examples/valid/expressions")
+  }
+
+  "function" should "succeed and print correct output" in {
+    assemblyRunFolder("wacc_examples/valid/function")
+  }
+
+  "if" should "succeed and print correct output" in {
+    assemblyRunFolder("wacc_examples/valid/if")
   }
 }
