050d9d532492509b9ec910cbc42c35c8f4093518
Joshua Young <jy2221@ic.ac.uk>
2023-02-27 21:35:10 +0000

    [jy2221] uncommented array tests and added extra detail to the test messages

--- src/test/scala/wacc/CodeGenTest/CodeGenTest.scala
+++ src/test/scala/wacc/CodeGenTest/CodeGenTest.scala
@@ -5,55 +5,55 @@ import wacc.helperFunction.assemblyRunFolder
 
 class CodeGenTest extends AnyFlatSpec {
 
-  "advanced" should "succeed and print correct output" in {
+  "advanced" should "succeed and print correct output for advanced" in {
     assemblyRunFolder("wacc_examples/valid/advanced")
   }
 
-//  "array" should "succeed and print correct output" in {
-//    assemblyRunFolder("wacc_examples/valid/array")
-//  }
+  "array" should "succeed and print correct output for array" in {
+    assemblyRunFolder("wacc_examples/valid/array")
+  }
 
-  "basic" should "succeed and print correct output" in {
+  "basic" should "succeed and print correct output for basic" in {
     assemblyRunFolder("wacc_examples/valid/basic")
   }
 
-  "expressions" should "succeed and print correct output" in {
+  "expressions" should "succeed and print correct output for expressions" in {
     assemblyRunFolder("wacc_examples/valid/expressions")
   }
 
-  "function" should "succeed and print correct output" in {
+  "function" should "succeed and print correct output for functions" in {
     assemblyRunFolder("wacc_examples/valid/function")
   }
 
-  "if" should "succeed and print correct output" in {
+  "if" should "succeed and print correct output for if" in {
     assemblyRunFolder("wacc_examples/valid/if")
   }
 
-  "IO" should "succeed and print correct output" in {
+  "IO" should "succeed and print correct output for IO" in {
     assemblyRunFolder("wacc_examples/valid/IO")
   }
 
-  "pairs" should "succeed and print correct output" in {
+  "pairs" should "succeed and print correct output for pairs" in {
     assemblyRunFolder("wacc_examples/valid/pairs")
   }
 
-  "runtimeErr" should "succeed and print correct output" in {
+  "runtimeErr" should "succeed and print correct output for runtimeErr" in {
     assemblyRunFolder("wacc_examples/valid/runtimeErr")
   }
 
-  "scope" should "succeed and print correct output" in {
+  "scope" should "succeed and print correct output for scope" in {
     assemblyRunFolder("wacc_examples/valid/scope")
   }
 
-  "sequence" should "succeed and print correct output" in {
+  "sequence" should "succeed and print correct output for sequence" in {
     assemblyRunFolder("wacc_examples/valid/sequence")
   }
 
-  "variables" should "succeed and print correct output" in {
+  "variables" should "succeed and print correct output for variables" in {
     assemblyRunFolder("wacc_examples/valid/variables")
   }
 
-  "while" should "succeed and print correct output" in {
+  "while" should "succeed and print correct output for while" in {
     assemblyRunFolder("wacc_examples/valid/while")
   }
 }
